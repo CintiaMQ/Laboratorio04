45ebc4e20496066d9f2b1b0029710a047059eb40
Alumno <Alumno@LAB1509-06>
2023-09-29 17:06:21 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio04/Laboratorio04/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Laboratorio04
+{
+    
+}
